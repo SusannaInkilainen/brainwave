02c742ba85792bc2e9ebb80b51b11b68b224674e
Sussu <inkilainensusanna@gmail.com>
2025-02-06 14:39:57 +0200

    full website

---
+++ brainwave/src/App.jsx
@@ -0,0 +1,35 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Benefits from "./components/Benefits.jsx";
+import Collaboration from "./components/Collaboration.jsx";
+import Services from "./components/Services.jsx";
+import Pricing from "./components/Pricing.jsx";
+import Roadmap from "./components/Roadmap.jsx";
+import Footer from "./components/Footer.jsx";
+
+
+const App = () => {
+
+  return (
+<>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Header />
+                <Hero />
+                <Benefits />
+                <Collaboration />
+                <Services />
+                <Pricing />
+                <Roadmap />
+                <Footer />
+
+            </div>
+    <ButtonGradient />
+
+</>
+
+  )
+}
+
+export default App
