c1eae80763e706e80447da87a3cdd9b6cb925baa
Sussu <inkilainensusanna@gmail.com>
2025-03-13 13:54:57 +0200

    tekstipäivityksiä

--- brainwave/src/components/Hero.jsx
+++ brainwave/src/components/Hero.jsx
@@ -21,12 +21,12 @@ const Hero = () => {
         <div className="container relative" ref={parallaxRef}>
             <div className="relative z-1 max-w-[62rem]
             mx-auto text-center mb-[3.875rem] md:mb lg:mb:[6rem]">
-                <h1 className="h1 mb-6">Explore the Possibilities of &nbsp;AI&nbsp;Chatting with {` `}
+                <h1 className="h1 mb-6">Tsiikaile mahdollisuuksia &nbsp;AI&nbsp; chattailyllä {` `}
                 <span className="inline-block relative"> Brainwave <img src={curve} className="absolute top-full left-0 w-full xl:-mt-2" width={624} height={28} alt="Curve"/>
                 </span></h1>
-                <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">Unleash the power of AI within Brainwave. Upgrade your productivity with Brainwave, the open AI chat app.</p>
+                <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">Vapauta tekoälyn voima käyttämällä Brainwave. Päivitä produktiivisuutesi uudelle tasolle.</p>
 
-                <Button href="/pricing" white> Get Started</Button>
+                <Button href="/#pricing" white>Aloita tästä</Button>
             </div>
             <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
                 <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
